79641edcfe0b4360a289a2d7fb91cae6f52aef2c
A3Alumno07 <luislopeznunosanchez@gmail.com>
2025-10-31 12:55:18 +0100

    Entrega de ejercicio

---
+++ Luis_Lopez-Nuno_Sanchez/src/ControlAcceso_Luis.java
@@ -0,0 +1,49 @@
+public class ControlAcceso_Luis {
+    private static final MyScanner sc = new MyScanner();
+
+    public static void main(String[] args) {
+
+        int edad = PedirEdad(); //Encapsulación
+        System.out.println(AnalisisEdad(edad));//Encapsulación
+    }
+
+
+    public static int PedirEdad() {
+        boolean salida = true;
+        int edad;
+
+
+        do {
+            edad = sc.pedirNumero("Ingrese su edad: ");//Scanner
+            for (int i = 1; i <= edad; i++) {
+                if (edad < 0 || edad > 120) {
+                    System.out.println("Error: Edad no valida.");
+                    salida = false;
+                    System.out.println("Intento " + i + " de 3");
+                    return i;
+                } else {
+                    salida = true;
+
+                }
+
+
+            }
+        }
+            while (!salida) ;//control de errrores
+            return edad;
+        }
+
+
+    public static String AnalisisEdad(int edad) {
+        String mensaje;
+        //condicionales
+        if (edad < 18) {
+            mensaje = "Acceso denegado: Menor de edad.";
+        } else if (edad < 65) {
+            mensaje = "Acceso permitido.";
+        } else {
+            mensaje = "Acceso con beneficios para jubilados";
+        }
+        return mensaje;
+    }
+}
---
+++ Luis_Lopez-Nuno_Sanchez/src/MyScanner.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class MyScanner {
+    private static Scanner sc;
+
+    public MyScanner() {
+        sc = new Scanner(System.in);
+    }
+    public int pedirNumero(String mns) {
+        int n = -1;
+        boolean flag = true;
+        while (flag) {
+            try {
+                System.out.println(mns);
+                n = sc.nextInt();
+                sc.nextLine();
+                flag = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Eso no es número!");
+                sc.nextLine();
+            }
+        }
+        return n;
+    }
+}
